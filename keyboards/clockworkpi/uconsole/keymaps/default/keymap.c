66772777b8d7a7044155ddc9b42ac43d21b921ce
perillamint <perillamint@silicon.moe>
2025-07-26 17:24:05 +0900

    Assign VolUp/Dn to Fn+bracket

--- keyboards/clockworkpi/uconsole/keymaps/default/keymap.c
+++ keyboards/clockworkpi/uconsole/keymaps/default/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, KC_LGUI, _______, KC_RGUI, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,
 
-        KC_PSCR, KC_PAUS, KC_MUTE, _______, _______, _______, KC_F11,  KC_F12,
+        KC_PSCR, KC_PAUS, KC_MUTE, _______, KC_VOLD, KC_VOLU, KC_F11,  KC_F12,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
         KC_F9,   KC_F10,  KB_LOCK, KC_CAPS, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_PGUP, KC_INS,
